7802ad9e9736b0cfce03130cea248be660ec3c66
Lukatiel <54953809+Lukatiel@users.noreply.github.com>
2022-03-07 19:32:56 -0500

    forgot about doxy wizard

---
+++ A1/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['aircraft_0',['Aircraft',['../class_aircraft.html',1,'']]],
+  ['aircraftmover_1',['AircraftMover',['../struct_aircraft_mover.html',1,'']]],
+  ['assignment_202_2',['Assignment 2',['../index.html',1,'']]]
+];
---
+++ A1/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['assignment_202_0',['Assignment 2',['../index.html',1,'']]]
+];
